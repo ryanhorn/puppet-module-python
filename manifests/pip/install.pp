900f7fd33abc86c3ed753355a05f69a96bb1b2be
Ryan Horn <ryan.horn.web@gmail.com>
2011-09-03 20:36:09 -0700

    Added timeout param

--- manifests/pip/install.pp
+++ manifests/pip/install.pp
@@ -1,5 +1,5 @@
 define python::pip::install($package, $venv, $ensure=present,
-                            $owner=undef, $group=undef) {
+                            $owner=undef, $group=undef, $timeout=300) {
 
   # Match against whole line if we provide a given version:
   $grep_regex = $package ? {
@@ -16,11 +16,13 @@ define python::pip::install($package, $venv, $ensure=present,
   if $ensure == 'present' {
     exec { "pip install $name":
       command => "$venv/bin/pip install $package",
-      unless => "$venv/bin/pip freeze | grep -e $grep_regex"
+      unless => "$venv/bin/pip freeze | grep -e $grep_regex",
+      timeout   => $timeout
     }
   } elsif $ensure == 'latest' {
     exec { "pip install $name":
       command => "$venv/bin/pip install -U $package",
+      timeout   => $timeout
     }
   } else {
     exec { "pip install $name":
